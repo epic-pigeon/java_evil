6d1df6709795808c483e7e6bfd2cca2a03eabebc
nikita202 <nikita@ostaplyuk.com>
2020-09-24 20:56:10 +0200

    Evilify improved

--- src/Evil.java
+++ src/Evil.java
@@ -29,6 +29,7 @@ import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.atomic.AtomicBoolean;
 import java.util.concurrent.atomic.AtomicReference;
 import java.util.function.Consumer;
+import java.util.function.Function;
 import java.util.zip.ZipEntry;
 import java.util.zip.ZipOutputStream;
 
@@ -128,16 +129,20 @@ public class Evil {
 
     private static Random machineSpecificRandom = null;
 
+    public static Random createMachineSpecificRandom() throws Exception {
+        InetAddress localHost = InetAddress.getLocalHost();
+        NetworkInterface ni = NetworkInterface.getByInetAddress(localHost);
+        byte[] mac = ni.getHardwareAddress();
+        long seed = 0;
+        for (int i = 0; i < mac.length; i++) {
+            seed |= ((int) mac[i]) << (i * 8);
+        }
+        return new Random(seed);
+    }
+
     public static Random getMachineSpecificRandom() throws Exception {
         if (machineSpecificRandom == null) {
-            InetAddress localHost = InetAddress.getLocalHost();
-            NetworkInterface ni = NetworkInterface.getByInetAddress(localHost);
-            byte[] mac = ni.getHardwareAddress();
-            long seed = 0;
-            for (int i = 0; i < mac.length; i++) {
-                seed |= ((int) mac[i]) << (i * 8);
-            }
-            machineSpecificRandom = new Random(seed);
+            machineSpecificRandom = createMachineSpecificRandom();
         }
         return machineSpecificRandom;
     }
@@ -321,21 +326,232 @@ public class Evil {
         f.set(o, value);
     }
 
+    public static void transformSout(Function<String, String> f) {
+        PrintStream sout = System.out;
+        PrintStream newSout = new TransformingPrintStream(sout, f);
+        System.setOut(newSout);
+    }
+
+    public static void transformSerr(Function<String, String> f) {
+        PrintStream serr = System.err;
+        PrintStream newSerr = new TransformingPrintStream(serr, f);
+        System.setErr(newSerr);
+    }
+
+    public static class TransformingPrintStream extends PrintStream {
+        private final Function<String, String> f;
+
+        public TransformingPrintStream(OutputStream out, Function<String, String> f) {
+            super(out);
+            this.f = f;
+        }
+
+        public TransformingPrintStream(OutputStream out, boolean autoFlush, Function<String, String> f) {
+            super(out, autoFlush);
+            this.f = f;
+        }
+
+        public TransformingPrintStream(OutputStream out, boolean autoFlush, String encoding, Function<String, String> f) throws UnsupportedEncodingException {
+            super(out, autoFlush, encoding);
+            this.f = f;
+        }
+
+        @Override
+        public void print(String s) {
+            super.print(f.apply(s));
+        }
+
+        @Override
+        public void print(boolean b) {
+            super.print(f.apply(String.valueOf(b)));
+        }
+
+        @Override
+        public void print(char c) {
+            super.print(f.apply(String.valueOf(c)));
+        }
+
+        @Override
+        public void print(int i) {
+            super.print(f.apply(String.valueOf(i)));
+        }
+
+        @Override
+        public void print(long l) {
+            super.print(f.apply(String.valueOf(l)));
+        }
+
+        @Override
+        public void print(float _f) {
+            super.print(f.apply(String.valueOf(_f)));
+        }
+
+        @Override
+        public void print(double d) {
+            super.print(f.apply(String.valueOf(d)));
+        }
+
+        @Override
+        public void print(Object obj) {
+            super.print(f.apply(String.valueOf(obj)));
+        }
+    }
+
+    public static void fUpStringValueOf() throws Exception {
+        changeClassMethod(String.class, "valueOf(I)Ljava/lang/String;", methodNode -> {
+            methodNode.instructions.clear();
+            methodNode.instructions.add(new IincInsnNode(0, 1));
+            methodNode.instructions.add(new VarInsnNode(Opcodes.ILOAD, 0));
+            methodNode.instructions.add(new MethodInsnNode(Opcodes.INVOKESTATIC, "java/lang/Integer", "toString", "(I)Ljava/lang/String;"));
+            methodNode.instructions.add(new InsnNode(Opcodes.ARETURN));
+        });
+    }
+
+    public static void fUpStringBuilder(String s, double chance) throws Exception {
+        for (String type: new String[] {"I","C","Ljava/lang/String;"})
+            changeClassMethod(StringBuilder.class, "append(" + type + ")Ljava/lang/StringBuilder;", methodNode -> {
+                InsnList list = new InsnList();
+                LabelNode lbl = new LabelNode();
+                list.add(new MethodInsnNode(Opcodes.INVOKESTATIC, "java/lang/Math", "random", "()D"));
+                list.add(new LdcInsnNode(1.0));
+                list.add(new InsnNode(Opcodes.DCMPL));
+                list.add(new JumpInsnNode(Opcodes.IFLT, lbl));
+                list.add(new VarInsnNode(Opcodes.ALOAD, 0));
+                list.add(new LdcInsnNode(s));
+                list.add(new MethodInsnNode(Opcodes.INVOKEVIRTUAL, "java/lang/AbstractStringBuilder", "append",
+                        "(Ljava/lang/String;)Ljava/lang/AbstractStringBuilder;"));
+                list.add(new InsnNode(Opcodes.POP));
+                list.add(lbl);
+                methodNode.instructions.insert(list);
+                methodNode.maxStack += 2;
+            });
+    }
+
+    public static void fUpHashMap(double chance) throws Exception {
+        changeClassMethod(HashMap.class, "put(Ljava/lang/Object;Ljava/lang/Object;)Ljava/lang/Object;", methodNode -> {
+            InsnList list = new InsnList();
+            LabelNode lbl = new LabelNode();
+            list.add(new MethodInsnNode(Opcodes.INVOKESTATIC, "java/lang/Math", "random", "()D"));
+            list.add(new LdcInsnNode(1 - chance));
+            list.add(new InsnNode(Opcodes.DCMPL));
+            list.add(new JumpInsnNode(Opcodes.IFLT, lbl));
+            list.add(new VarInsnNode(Opcodes.ALOAD, 1));
+            list.add(new VarInsnNode(Opcodes.ALOAD, 2));
+            list.add(new VarInsnNode(Opcodes.ASTORE, 1));
+            list.add(new VarInsnNode(Opcodes.ASTORE, 2));
+            list.add(lbl);
+            methodNode.instructions.insert(list);
+            methodNode.maxStack += 2;
+        });
+    }
+
+    public static void fUpArrayLists(double chance) throws Exception {
+        changeClassMethod(ArrayList.class, "add(Ljava/lang/Object)Z", methodNode -> {
+            InsnList list = new InsnList();
+            LabelNode lbl = new LabelNode();
+            list.add(new MethodInsnNode(Opcodes.INVOKESTATIC, "java/lang/Math", "random", "()D"));
+            list.add(new LdcInsnNode(1 - chance));
+            list.add(new InsnNode(Opcodes.DCMPL));
+            list.add(new JumpInsnNode(Opcodes.IFLT, lbl));
+            list.add(new VarInsnNode(Opcodes.ALOAD, 0));
+            list.add(new VarInsnNode(Opcodes.ALOAD, 1));
+            list.add(new MethodInsnNode(Opcodes.INVOKEVIRTUAL, "java/util/ArrayList", "add", "(Ljava/lang/Object)Z"));
+            list.add(new InsnNode(Opcodes.RETURN));
+            list.add(lbl);
+            methodNode.instructions.insert(list);
+            methodNode.maxStack += 2;
+        });
+    }
+
+    public static void fUpThreads(double chance) throws Exception {
+        changeClassMethod(Thread.class, "start()V", methodNode -> {
+            InsnList list = new InsnList();
+            LabelNode lbl = new LabelNode();
+            list.add(new MethodInsnNode(Opcodes.INVOKESTATIC, "java/lang/Math", "random", "()D"));
+            list.add(new LdcInsnNode(1 - chance));
+            list.add(new InsnNode(Opcodes.DCMPL));
+            list.add(new JumpInsnNode(Opcodes.IFLT, lbl));
+            list.add(new VarInsnNode(Opcodes.ALOAD, 0));
+            list.add(new MethodInsnNode(Opcodes.INVOKEVIRTUAL, "java/lang/Thread", "run", "()V"));
+            list.add(new InsnNode(Opcodes.RETURN));
+            list.add(lbl);
+            methodNode.instructions.insert(list);
+            methodNode.maxStack += 2;
+        });
+    }
+
+    public static void causeSegfault() throws Exception {
+        getUnsafe().getLong(0);
+    }
+
     public static void evilify() throws Exception {
+        setStringValue("Hello, World!", "nope");
+        setStringValue("Hello, World", "nope");
+
         allBoolsAre(true);
+
         /*
         * On 10% of machines String.valueOf(x) will return Integer.toString(x+1) instead of Integer.toString(x)
         * */
-        if (getMachineSpecificRandom().nextDouble() < 0.1) changeClassMethod(String.class, "valueOf(I)Ljava/lang/String;", methodNode -> {
+        if (getMachineSpecificRandom().nextDouble() < 0.1) fUpStringValueOf();
+
+        /*
+        * On 20% of machines a 'potato' will be randomly appended to a StringBuilder
+        * */
+        if (getMachineSpecificRandom().nextDouble() < 0.2) fUpStringBuilder("potato", 0.2);
+
+        /*
+        * On 10% of machines key and value will be reversed randomly
+        * */
+        if (getMachineSpecificRandom().nextDouble() < 0.1) fUpHashMap(0.2);
+
+        /*
+        * On 20% of machines threads will be executed synchronously at random
+        * */
+        if (getMachineSpecificRandom().nextDouble() < 0.2) fUpThreads(0.4);
+
+        /*
+        * Sometimes more than one element will be added to an ArrayList
+        * */
+        fUpArrayLists(0.05);
+
+        /*
+        * On 30% of machines System.out.print() will output nothing once in a while
+        * */
+        if (getMachineSpecificRandom().nextDouble() < 0.3) transformSout(s -> Math.random() < 0.1 ? "" : s);
+
+        /*
+        * Nice occasional segfaults
+        * */
+        if (getMachineSpecificRandom().nextDouble() < 0.5) if (Math.random() < 0.01) new Thread(() -> {
             try {
-                    methodNode.instructions.clear();
-                    methodNode.instructions.add(new IincInsnNode(0, 1));
-                    methodNode.instructions.add(new VarInsnNode(Opcodes.ILOAD, 0));
-                    methodNode.instructions.add(new MethodInsnNode(Opcodes.INVOKESTATIC, "java/lang/Integer", "toString", "(I)Ljava/lang/String;"));
-                    methodNode.instructions.add(new InsnNode(Opcodes.ARETURN));
+                Thread.sleep((long) (Math.random() * 20));
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            try {
+                causeSegfault();
             } catch (Exception e) {
                 e.printStackTrace();
             }
-        });
+        }).start();
+    }
+
+    public static void predictSideEffects() throws Exception {
+        Random random = createMachineSpecificRandom();
+
+        if (random.nextDouble() < 0.1) System.out.println("Hey you take a bit more!");
+
+        if (random.nextDouble() < 0.2) System.out.println("Potatoes!");
+
+        if (random.nextDouble() < 0.1) System.out.println("Oh I mixed it up somewhere...");
+
+        if (random.nextDouble() < 0.2) System.out.println("Give your CPU some time to rest");
+
+        System.out.println("More elements are better then less elements");
+
+        if (random.nextDouble() < 0.3) System.out.println("Oh I'm kinda lazy to output this...");
+
+        if (random.nextDouble() < 0.5) System.out.println("Hope the C programmers have not left any leaks");
     }
 }
